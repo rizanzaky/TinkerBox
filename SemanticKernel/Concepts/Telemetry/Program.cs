00db5c15603658d79827223dfb2574125efc044f
rizanzaky <syncrizan@gmail.com>
2025-02-07 10:18:45 +0530

    semantic kernel observability

---
+++ SemanticKernel/Concepts/Telemetry/Program.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Telemetry;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+    .AddUserSecrets<Program>()
+    .Build();
+
+var aspireTelemetryEndpoint = "http://localhost:4317";
+
+// resource
+var resourceBuilder = ResourceBuilder
+    .CreateDefault()
+    .AddService("ConsoleAndAspireTelemetry");
+
+// switch
+AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", false);
+AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnostics", true);
+
+// providers
+using (Sdk.CreateTracerProviderBuilder()
+    .SetResourceBuilder(resourceBuilder)
+    .AddSource("Microsoft.SemanticKernel*")
+    .AddConsoleExporter()
+    .AddOtlpExporter(options => options.Endpoint = new Uri(aspireTelemetryEndpoint))
+    .Build())
+using (Sdk.CreateMeterProviderBuilder()
+    .SetResourceBuilder(resourceBuilder)
+    .AddMeter("Microsoft.SemanticKernel*")
+    .AddConsoleExporter()
+    .AddOtlpExporter(options => options.Endpoint = new Uri(aspireTelemetryEndpoint))
+    .Build())
+{
+    using var loggerFactory = LoggerFactory.Create(builder =>
+    {
+        // log provider
+        builder.AddOpenTelemetry(options =>
+        {
+            options.SetResourceBuilder(resourceBuilder);
+            options.AddConsoleExporter();
+            options.AddOtlpExporter(options => options.Endpoint = new Uri(aspireTelemetryEndpoint));
+            options.IncludeFormattedMessage = true;
+            options.IncludeScopes = true;
+        });
+        builder.SetMinimumLevel(LogLevel.Information);
+    });
+
+    var config = new
+    {
+        DeploymentName = configuration["SemanticKernel:DeploymentName"],
+        Endpoint = configuration["SemanticKernel:Endpoint"],
+        ApiKey = configuration["SemanticKernel:ApiKey"]
+    };
+
+    var builder = Host.CreateApplicationBuilder(args);
+
+    builder.Services.AddAzureOpenAIChatCompletion(config.DeploymentName, config.Endpoint, config.ApiKey);
+
+    builder.Services.AddSingleton(loggerFactory);
+
+    builder.Services.AddTransient((serviceProvider) =>
+    {
+        var kernel = new Kernel(serviceProvider);
+        return kernel;
+    });
+
+    builder.Services.AddHostedService<Worker>();
+
+    using var host = builder.Build();
+
+    await host.RunAsync();
+}
---
+++ SemanticKernel/Concepts/Telemetry/Worker.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace Telemetry;
+
+public class Worker : BackgroundService
+{
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+    private readonly Kernel _kernel;
+
+    public Worker(IHostApplicationLifetime hostApplicationLifetime, Kernel kernel)
+    {
+        _hostApplicationLifetime = hostApplicationLifetime;
+        _kernel = kernel;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var history = new ChatHistory();
+
+        var chat = _kernel.GetRequiredService<IChatCompletionService>();
+
+        var input = "";
+        do
+        {
+            Console.Write("You: ");
+            input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.Write($"\nAgent: Mmm... you didn't say anything!\n\n");
+                continue;
+            }
+
+            history.AddUserMessage(input);
+
+            var result = await chat.GetChatMessageContentAsync(history, kernel: _kernel);
+            if (!string.IsNullOrWhiteSpace(result.Content))
+            {
+                Console.Write($"\nAgent: {result.Content}\n\n");
+                history.Add(result);
+                continue;
+            }
+
+            history.Add(result);
+        }
+        while (input?.ToLower() != "exit");
+
+        _hostApplicationLifetime.StopApplication();
+    }
+}
